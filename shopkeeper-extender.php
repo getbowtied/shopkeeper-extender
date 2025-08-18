9d155f3fa8d9c9c91b2d898879a0fdf9b1630ecd
Romi H. <romi@getbowtied.com>
2025-08-18 16:16:42 +0300

    6.0

--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -4,7 +4,7 @@
  * Plugin Name:       		Shopkeeper Extender
  * Plugin URI:        		https://shopkeeper.wp-theme.design/
  * Description:       		Extends the functionality of Shopkeeper with theme specific features.
- * Version:           		5.9
+ * Version:           		6.0
  * Author:            		Get Bowtied
  * Author URI:				https://getbowtied.com
  * Text Domain:				shopkeeper-extender
