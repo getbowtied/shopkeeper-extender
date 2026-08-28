3c65c65e258b2c7ac7f9d4da33ff1481590f0d0a
Romi H. <romi@getbowtied.com>
2026-08-28 12:30:15 +0300

    theme updater

--- shopkeeper-extender.php
+++ shopkeeper-extender.php
@@ -38,6 +38,8 @@ if ( ! class_exists( 'ShopkeeperExtender' ) ) :
 				return;
 			}
 
+			require_once dirname( __FILE__ ) . '/core/theme-updater/class-gbt-extender-theme-updater.php';
+
 			if ( ! function_exists( 'is_plugin_active' ) ) {
 				require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
 			}
